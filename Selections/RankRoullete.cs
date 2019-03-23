00d6670e71263adbf5e5e2e26ca996cf566ab4b0
Maciej Małaszek <maciej.malaszek@predica.pl>
2019-03-23 13:33:16 +0100

    Added RankRoullete & fixed RoulleteWheel

--- Selections/RankRoullete.cs
+++ Selections/RankRoullete.cs
@@ -2,34 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using GeneticToolkit.Interfaces;
+using GeneticToolkit.Utils.Extensions;
 
 namespace GeneticToolkit.Selections
 {
-    /// <summary>
-    /// Selection method that assures best individuals have highest chance of selection.
-    /// One of slowest, because of more arithmetic operations
-    /// </summary>
-    public class RouletteWheel : ISelectionMethod
+    public class RankRoullete : ISelectionMethod
     {
         public ICompareCriteria CompareCriteria { get; set; }
+
+        public Func<int, double> RankingValueFunc;
 
         protected Random RandomNumberGenerator { get; set; } = new Random();
 
         protected IPopulation Population { get; set; } = null;
 
-        protected IList<double> FitnessList = new List<double>();
+        protected List<IIndividual> SortedList = new List<IIndividual>();
 
         protected uint CurrentGeneration { get; set; }
 
         protected bool Deprecated { get; set; } = true;
 
+        protected IList<double> FitnessList = new List<double>();
+       
         protected double? MinValue { get; set; }
 
         protected double Sum { get; set; } = 0;
 
+
+        public RankRoullete(ICompareCriteria compareCriteria, Func<int, double> rankingValueFunc)
+        {
+            CompareCriteria = compareCriteria;
+            RankingValueFunc = rankingValueFunc;
+        }
+
         public IIndividual Select(IPopulation population)
         {
             Deprecated = population != Population || population.Generation != CurrentGeneration;
+
             if(Deprecated)
                 Update(population);
 
@@ -37,32 +46,39 @@ namespace GeneticToolkit.Selections
             int iterator = 0;
             double localSum = FitnessList[iterator] - MinValue.Value;
 
-            while (localSum < randomValue && iterator < Population.Size-1)
+            while(localSum < randomValue && iterator < Population.Size - 1)
                 localSum += FitnessList[++iterator] - MinValue.Value;
 
             return Population[iterator];
-
         }
 
-        private void Update(IPopulation population)
+        public void Update(IPopulation population)
         {
             Population = population;
             CompareCriteria = Population.CompareCriteria;
-            CurrentGeneration = population.Generation;
-            MinValue = null;
+            CurrentGeneration = Population.Generation;
             FitnessList.Clear();
-            
-            foreach(IIndividual ind in Population)
+            SortedList = Population.ToList();
+            SortedList.Sort( (individual, individual1) =>
             {
-                double functionValue = CompareCriteria.FitnessFunction.GetValue(ind.Phenotype);
-                if(functionValue < MinValue || MinValue.HasValue == false)
+                var result = CompareCriteria.Compare(individual, individual1);
+                return result;
+            });
+
+            MinValue = null;
+            for(int i = 0; i < SortedList.Count; i++)
+            {
+                double functionValue = RankingValueFunc(i);
+                if(functionValue < MinValue || MinValue == null)
                     MinValue = functionValue;
                 FitnessList.Add(functionValue);
             }
-            Deprecated = false;
+
             Sum = FitnessList.Sum(x => x - MinValue.Value);
-            
+
+            Deprecated = false;
         }
+
 
     }
 }
