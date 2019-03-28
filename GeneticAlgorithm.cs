cf3d5ef1fce85358c0860739a9f8f2426f431355
Maciej Małaszek <maciej.malaszek@predica.pl>
2019-03-28 11:16:29 +0100

    Added GA class

---
+++ GeneticAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using GeneticToolkit.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticToolkit
+{
+    public enum EStopConditionMode { Any, All }
+
+    public class GeneticAlgorithm
+    {
+        public IPopulation Population { get; set; }
+
+        public ICollection<IStopCondition> StopConditions { get; set; }
+
+        public EStopConditionMode StopConditionMode { get; set; } = EStopConditionMode.Any;
+
+        public void Run()
+        {
+            switch (StopConditionMode)
+            {
+                case EStopConditionMode.Any:
+                    while (StopConditions.Any(x => x.Satisfied(Population)) == false)
+                        Population.NextGeneration();
+                    break;
+                case EStopConditionMode.All:
+                    while (StopConditions.All(x => x.Satisfied(Population)) == false)
+                        Population.NextGeneration();
+                    break;
+                default: return;
+            }
+        }
+
+        public void Reset()
+        {
+            Population.Initialize();
+            foreach (var stopCondition in StopConditions)
+                stopCondition.Reset();
+        }
+
+
+
+    }
+}
