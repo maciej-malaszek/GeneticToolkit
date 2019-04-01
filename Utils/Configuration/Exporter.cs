c69518a4bd681d957a697bfe926d180a841bc168
Maciej Małaszek <malasz95@gmail.com>
2019-04-01 11:13:48 +0200

    Fixed Population Import

---
+++ Utils/Configuration/Exporter.cs
@@ -0,0 +1,11 @@
+﻿using GeneticToolkit.Utils.Data;
+
+namespace GeneticToolkit.Utils.Configuration
+{
+    public class Exporter
+    {
+        public static void ExportSettings(GeneticAlgorithmSettings settings, string path)
+        {
+        }
+    }
+}
---
+++ Utils/Data/GeneticAlgorithmParameter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeneticToolkit.Interfaces;
+
+namespace GeneticToolkit.Utils.Data
+{
+    public class GeneticAlgorithmParameter
+    {
+        // Required by JsonConvert.Deserialize
+        public GeneticAlgorithmParameter()
+        {
+        }
+
+        public GeneticAlgorithmParameter(IGeneticSerializable serializableObject)
+        {
+            var type = serializableObject.GetType();
+            Type = type.IsGenericType ? type.GetGenericTypeDefinition().FullName : type.FullName;
+            GenericArguments = type.GenericTypeArguments.Select(x => x.FullName).ToArray();
+        }
+
+        public string Type { get; set; }
+
+        public string[] GenericArguments { get; set; }
+
+        public IDictionary<string, object> Params { get; set; } = new Dictionary<string, object>();
+    }
+}
