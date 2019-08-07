b9594a4dba3ffb660788e77ae0655e29ac80c769
Maciej <malasz95@gmail.com>
2019-08-07 17:29:37 +0200

    Added Mutation Policies: Back, Back-Schutz, Hesser-Manner

---
+++ GeneticToolkit/Policies/Mutation/BackSchutzMutation.cs
@@ -0,0 +1,26 @@
+﻿using GeneticToolkit.Interfaces;
+
+using System;
+
+namespace GeneticToolkit.Policies.Mutation
+{
+    public class BackSchutzMutation : IMutationPolicy
+    {
+        public float GetMutationChance(IPopulation population)
+        {
+            return (float)(1.0/(2+CalculationsTotalTime*(population[0].Genotype.Length*8-2)/CalculationsTotalTime));
+        }
+
+        public float CalculationsTotalTime { get; set; }
+        public float CalculationTime { get; set; }
+        public float MutatedGenesPercent { get; }
+
+        public Func<float> GetCalculationTime { get; set; }
+
+        public BackSchutzMutation(float calculationsTotalTime, Func<float> getCalculationTime)
+        {
+            CalculationsTotalTime = calculationsTotalTime;
+            GetCalculationTime = getCalculationTime;
+        }
+    }
+}
---
+++ GeneticToolkit/Policies/Mutation/HesserMannerMutation.cs
@@ -0,0 +1,25 @@
+﻿using GeneticToolkit.Interfaces;
+
+using System;
+
+namespace GeneticToolkit.Policies.Mutation
+{
+    public class HesserMannerMutation : IMutationPolicy
+    {
+        private float _alpha;
+        public float Alpha { get => _alpha; set => _alpha = Math.Abs(value); }
+        public float Beta { get; set; }
+        public float GetMutationChance(IPopulation population)
+        {
+            return (float)(Alpha * Math.Pow(Math.E, -Beta * population.Generation / 2.0) / (population.Size * Math.Sqrt(population[0].Genotype.Length*8)));
+        }
+        public float MutatedGenesPercent { get; }
+
+        public HesserMannerMutation(float alpha, float beta, float mutatedGenesPercent)
+        {
+            MutatedGenesPercent = mutatedGenesPercent;
+            Alpha = Math.Abs(alpha);
+            Beta = beta;
+        }
+    }
+}
